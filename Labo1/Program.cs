fbe7ad5513f3105a8da190045522e13313d680a1
MaelMbange <maelmbange21@gmail.com>
2023-02-14 10:16:49 +0100

    :tada: First commit

---
+++ Labo1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Labo1
+{
+    internal class Program
+    {
+        public static void Main(String[] argv)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
