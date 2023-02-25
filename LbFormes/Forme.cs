15e4e07a383d2cf79594552248bdc05918ca4994
MaelMbange <maelmbange21@gmail.com>
2023-02-25 19:35:05 +0100

    :bento:"Affiche" method added

---
+++ LbFormes/Forme.cs
@@ -0,0 +1,18 @@
+﻿namespace LbFormes;
+
+public abstract class Forme
+{
+    protected Coordonnees _coordonnees;
+
+    public Forme()
+    {
+        _coordonnees = new Coordonnees();
+    }
+
+    protected Forme(Coordonnees coordonnees)
+    {
+        _coordonnees = coordonnees;
+    }
+
+    public abstract void Affiche();
+}
