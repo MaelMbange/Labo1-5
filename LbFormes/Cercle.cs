2067cab40dace89b98ed062694fab3958e522658
MaelMbange <maelmbange21@gmail.com>
2023-02-26 01:42:50 +0100

    Creation Classes Cercle-Rectangle

---
+++ LbFormes/Cercle.cs
@@ -0,0 +1,18 @@
+﻿namespace LbFormes;
+
+public class Cercle : Forme
+{
+    private float _rayon;
+    public float Rayon { get; set; }
+
+    public Cercle() : this(0,0,0){}
+    public Cercle(float rayon, int x, int y) => (_rayon, _coordonnees.X, _coordonnees.Y) = (rayon, x, y);
+
+    
+    public override void Affiche()
+    {
+        Console.WriteLine(ToString());
+    }
+
+    public override string ToString() => _coordonnees.X + " : " + _coordonnees.Y + " | ray = " + _rayon;
+}
---
+++ LbFormes/Rectange.cs
@@ -0,0 +1,22 @@
+﻿namespace LbFormes;
+
+public class Rectange : Forme
+{
+    private float _largeur;
+    private float _longueur;
+
+    public float Longeur { get; set; }
+    public float Largeur { get; set; }
+
+
+    public Rectange() : this(0, 0, 0, 0){}
+    public Rectange(float largeur, float longueur, int x, int y) =>
+        (_longueur, _largeur, _coordonnees.X, _coordonnees.Y) = (longueur, largeur, x, y);
+    
+    public override void Affiche()
+    {
+        Console.WriteLine(ToString());
+    }
+
+    public override string ToString() => _coordonnees.X + " : " + _coordonnees.Y + " | lenght = " + _longueur + " | width = "+ _largeur;
+}
