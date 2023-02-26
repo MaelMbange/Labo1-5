b19f6a5d188163316ff7cfb0a0270a6d98234bad
MaelMbange <maelmbange21@gmail.com>
2023-02-26 01:17:12 +0100

    Ajout Class Carre

---
+++ LbFormes/Carre.cs
@@ -0,0 +1,20 @@
+﻿namespace LbFormes;
+
+public class Carre : Forme
+{
+    private float _lenght;
+    public float Lenght { get; set; }
+
+    public Carre() : this(0, 0,0){}
+    public Carre(float lenght, int x, int y) : base(x, y) => _lenght = lenght;
+    public Carre(float lenght,Coordonnees coordonnees) : this(lenght,coordonnees.X,coordonnees.Y){}
+
+    public override void Affiche()
+    {
+        Console.WriteLine(ToString());
+    }
+    public override string ToString()
+    {
+        return _coordonnees.X.ToString() + " : " + _coordonnees.Y.ToString() + " | lenght = " + _lenght.ToString();
+    }
+}
